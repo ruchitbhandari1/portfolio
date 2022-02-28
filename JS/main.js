3001ec41c5cd6d940f772f0d69c8260ace8447a9
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2022-03-01 04:55:38 +0530

    Hamburger closes when clicking any of the links in it.

--- JS/main.js
+++ JS/main.js
@@ -1,34 +1,51 @@
-const hamburger = document.querySelector('.hamburger');
+const hamburger = document.querySelector(".hamburger");
+const open = document.querySelector(".open");
+const close = document.querySelector(".close");
 const popup = document.querySelector("#popup");
+const navItems = document.querySelector(".ham-nav-items");
 
-hamburger.addEventListener('click', function (){
-    this.classList.toggle("is-active");
-    document.querySelector("#navigation").classList.toggle("hide");
-});
+const openMenu = function () {
+  // this.classList.toggle("is-active");
+  open.classList.add("hide");
+  close.classList.remove("hide");
+  document.querySelector("#navigation").classList.remove("hide");
+};
+const closeMenu = function () {
+  // this.classList.toggle("is-active");
+  open.classList.remove("hide");
+  close.classList.add("hide");
+  document.querySelector("#navigation").classList.add("hide");
+};
 
-function sendEmail(){
-    var greet = "Message sent"
-    Email.send({
-      Host: "smtp.gmail.com",
-      Username: "ruchitbhandari7@gmail.com",
-      Password: "poavonlyswuofwzn",
-      To: "ruchitbhandari1@gmail.com",
-      From: document.getElementById("email").value,
-      Subject: document.getElementById("subject").value,
-      Body:
-        "Name:- " +
-        document.getElementById("name").value +
-        "<br>Email:- " +
-        document.getElementById("email").value +
-        "<br>Subject:- " +
-        document.getElementById("subject").value +
-        "<br>Message:- " +
-        document.getElementById("message").value,
-    }).then(() => {
-        popup.classList.remove("hidden");
-        popup.classList.add("anime");
-        setTimeout(function () {
-          popup.classList.add("hidden");
-        }, 4000);
-    });
+open.addEventListener("click", openMenu);
+
+close.addEventListener("click", closeMenu);
+
+navItems.addEventListener("click", closeMenu);
+
+function sendEmail() {
+  var greet = "Message sent";
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "ruchitbhandari7@gmail.com",
+    Password: "poavonlyswuofwzn",
+    To: "ruchitbhandari1@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: document.getElementById("subject").value,
+    Body:
+      "Name:- " +
+      document.getElementById("name").value +
+      "<br>Email:- " +
+      document.getElementById("email").value +
+      "<br>Subject:- " +
+      document.getElementById("subject").value +
+      "<br>Message:- " +
+      document.getElementById("message").value,
+  }).then(() => {
+    popup.classList.remove("hidden");
+    popup.classList.add("anime");
+    setTimeout(function () {
+      popup.classList.add("hidden");
+    }, 4000);
+  });
 }
