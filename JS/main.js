947b8b6a3b2c50b9716208c12fb3d7156a5e2474
Ruchit Bhandari <ruchitbhandari1@gmail.com>
2022-03-10 06:06:26 +0530

    Clicking anywhere closes navigation menu

--- JS/main.js
+++ JS/main.js
@@ -3,23 +3,28 @@ const open = document.querySelector(".open");
 const close = document.querySelector(".close");
 const popup = document.querySelector("#popup");
 const navItems = document.querySelector(".ham-nav-items");
+const overlay = document.querySelector(".overlay");
 
 const openMenu = function () {
   // this.classList.toggle("is-active");
   open.classList.add("hide");
   close.classList.remove("hide");
+  overlay.classList.remove("hide");
   document.querySelector("#navigation").classList.remove("hide");
 };
 const closeMenu = function () {
   // this.classList.toggle("is-active");
   open.classList.remove("hide");
   close.classList.add("hide");
+  overlay.classList.add("hide");
   document.querySelector("#navigation").classList.add("hide");
 };
 
 open.addEventListener("click", openMenu);
 
 close.addEventListener("click", closeMenu);
+
+overlay.addEventListener("click", closeMenu);
 
 navItems.addEventListener("click", closeMenu);
 
